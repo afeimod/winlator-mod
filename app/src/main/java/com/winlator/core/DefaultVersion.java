7c6e023fac93ca7bc196e26fb17331371fd7c2fe
BrunoSX <brunodeveloper85@gmail.com>
2025-10-06 13:59:12 -0300

    app: Update graphics driver turnip

--- app/src/main/java/com/winlator/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/core/DefaultVersion.java
@@ -4,7 +4,7 @@ import com.winlator.container.GraphicsDrivers;
 
 public abstract class DefaultVersion {
     public static final String BOX64 = "0.3.6";
-    public static final String TURNIP = "25.1.0";
+    public static final String TURNIP = "25.2.0";
     public static final String VORTEK = "2.1";
     public static final String ZINK = "22.2.5";
     public static final String VIRGL = "23.1.9";
